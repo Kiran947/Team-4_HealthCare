09073de9e822a4ec732dbd304da3c1e5f852d880
Kiran <Kirankumar.pannala@saintleo.Edu>
2023-04-01 14:08:03 -0400

    Login json Data Storage and Class Creation

---
+++ Team-4_HealthCare/loginClass.cs
@@ -0,0 +1,12 @@
+﻿namespace Kiran
+{
+    public class LoginarrayList
+    {
+        public LoginList[]? LoginLists { get; set; }
+    }
+    public class LoginList
+    {
+        public string? userName { get; set; }
+        public string? password { get; set; }
+    }
+}
